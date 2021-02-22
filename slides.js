00a931256da5aa78363e8d23cec3b392d09dbcae
SANSWET920 <53849001+SANSWET920@users.noreply.github.com>
2021-02-22 10:57:56 +0530

    Add files via upload

---
+++ slides.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        id: '1',
+        title: 'Quick & Easy Payments',
+        description:'Grow your buisness by accepting card payments with the new card reader.',
+        image: required('./assets/images/creditcard.png'),
+    },
+    {
+        id: '2',
+        title: 'Smart Point of Sale',
+        description: 'Complete point of sale software tailored to your buisness needs.',
+        image: required('./assets/images/computer.png'),
+    },
+    {
+        id: '3',
+        title: 'instant Notification',
+        description: 'Instant notification let your quickly see new purchases and messages.',
+        image: require('./assets/images/notifications.png'),
+    },
+    {
+        id: '4',
+        title: 'customize Everything',
+        description: 'Adjust your system to speed up your checkout.',
+        image: required('./assets/images/customize.png'),
+    },
+
+];
